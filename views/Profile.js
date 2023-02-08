1acb76458409759e14f49cf0bcb346f741e7d2e6
shilpasy <shilpa.yadav@metropolia.fi>
2023-02-08 09:58:11 +0200

    Task B started

--- views/Profile.js
+++ views/Profile.js
@@ -1,14 +1,12 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {Button, ScrollView} from 'react-native';
 import {MainContext} from '../contexts/MainContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useTag} from '../hooks/ApiHooks';
 import {uploadsUrl} from '../utils/variables';
-import {Card, Icon, ListItem} from '@rneui/themed';
-import EditProfileForm from '../components/EditProfileForm';
-import EditPasswordForm from '../components/EditPasswordForm';
+import {Button, Card, Icon, ListItem} from '@rneui/themed';
+import PropTypes from 'prop-types';
 
-const Profile = () => {
+const Profile = ({navigation}) => {
   const {getFilesByTag} = useTag();
   const {setIsLoggedIn, user, setUser} = useContext(MainContext);
   const [avatar, setAvatar] = useState('');
@@ -27,36 +25,42 @@ const Profile = () => {
   }, []);
 
   return (
-    <ScrollView>
-      <Card>
-        <Card.Title>{user.username}</Card.Title>
-        <Card.Image source={{uri: uploadsUrl + avatar}} />
-        <ListItem>
-          <Icon name="email" />
-          <ListItem.Title>{user.email}</ListItem.Title>
-        </ListItem>
-        <ListItem>
-          <Icon name="badge" />
-          <ListItem.Title>{user.full_name}</ListItem.Title>
-        </ListItem>
-        <Button
-          title="Logout!"
-          onPress={async () => {
-            console.log('Logging out!');
-            setUser({});
-            setIsLoggedIn(false);
-            try {
-              await AsyncStorage.clear();
-            } catch (error) {
-              console.error('clearing asyncstorage failed', error);
-            }
-          }}
-        />
-      </Card>
-      <EditProfileForm />
-      <EditPasswordForm />
-    </ScrollView>
+    <Card>
+      <Card.Title>{user.username}</Card.Title>
+      <Card.Image source={{uri: uploadsUrl + avatar}} />
+      <ListItem>
+        <Icon name="email" />
+        <ListItem.Title>{user.email}</ListItem.Title>
+      </ListItem>
+      <ListItem>
+        <Icon name="badge" />
+        <ListItem.Title>{user.full_name}</ListItem.Title>
+      </ListItem>
+      <Button
+        title="Logout!"
+        onPress={async () => {
+          console.log('Logging out!');
+          setUser({});
+          setIsLoggedIn(false);
+          try {
+            await AsyncStorage.clear();
+          } catch (error) {
+            console.error('clearing asyncstorage failed', error);
+          }
+        }}
+      />
+      <Button
+        title="My Files"
+        onPress={() => {
+          navigation.navigate('MyFiles');
+        }}
+      />
+    </Card>
   );
+};
+
+Profile.propTypes = {
+  navigation: PropTypes.object,
 };
 
 export default Profile;
